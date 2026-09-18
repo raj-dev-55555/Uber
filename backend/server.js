08b17a7a4201dc87979f3b2f76f4854a74568627
raj-dev-55555 <vt3165861@gmail.com>
2026-09-18 17:31:24 +0530

    change port

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const http = require("http");//Ye server banane ka low-level way deta hai.Express internally isi HTTP functionality ke upar kaam karta hai.
 const app = require("./app");
-const port = process.env.port||3000;
+const port = process.env.PORT||3000;
 const { initializeSocket } = require('./socket');
 
 
